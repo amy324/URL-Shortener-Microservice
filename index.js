f6b053b150b8c62bdf71b331b842ec1fbbb7703f
amy-monroe <amy.monroe97@gmail.com>
2023-12-08 15:54:46 +0000

    added index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const validUrl = require('valid-url'); 
+const shortid = require('shortid');
 const app = express();
 
-// Basic Configuration
+
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -17,6 +20,33 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+// URL Shortener Logic
+const urlDatabase = {};
+const baseUrl = 'https://your-base-url.com'; 
+
+app.post('/api/shorturl', function(req, res) {
+  const originalUrl = req.body.url;
+
+  if (!validUrl.isWebUri(originalUrl)) {
+    res.json({ error: 'invalid url' });
+  } else {
+    const shortUrl = shortid.generate();
+    urlDatabase[shortUrl] = originalUrl;
+    res.json({ original_url: originalUrl, short_url: shortUrl });
+  }
+});
+
+app.get('/api/shorturl/:short_url', function(req, res) {
+  const shortUrl = req.params.short_url;
+
+  if (urlDatabase.hasOwnProperty(shortUrl)) {
+    const originalUrl = urlDatabase[shortUrl];
+    res.redirect(originalUrl);
+  } else {
+    res.json({ error: 'short URL not found' });
+  }
 });
 
 app.listen(port, function() {
